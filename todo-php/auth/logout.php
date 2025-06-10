82a90bd5e2596327d96a5a226bd95caae601a1cf
amirpeyravan <141130613+AmirPeyravan@users.noreply.github.com>
2025-06-10 12:08:14 +0330

    Update logout.php

--- todo-php/auth/logout.php
+++ todo-php/auth/logout.php
@@ -1,18 +1,1 @@
-<?php
-session_start();
 
-$result = $_SESSION['useraccount'];
-
-function logOut(string $username)
-{
-    if (session_status() === PHP_SESSION_ACTIVE) {
-        if (isset($_SESSION['useraccount']) && $_SESSION['useraccount'] === $username) {
-            session_unset();
-            session_destroy();
-            header("Location: http://localhost/todo-php/auth/login.html.php");
-            exit();
-        }
-    }
-}
-logOut($result);
-?>
